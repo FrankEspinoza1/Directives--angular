d204266079c6434b1cc9a0ab3c70f95ee9017cea
FranklinEspinoza1 <franklin.espinozae02@gmail.com>
2023-01-09 20:41:25 -0500

    subiendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sesion9';
+  num1: number = 0;
+  num2: number = 0;
+  operacion: string = 'defecto';
+
+  resultado: number = 0;
+
+  onSumar(): void {
+    this.resultado = this.num1 + this.num2;
+  }
+  onRestar(): void {
+    this.resultado = this.num1 - this.num2;
+  }
+  onMultiplicar(): void {
+    this.resultado = this.num1 * this.num2;
+  }
+  onDividir(): void {
+    this.resultado = this.num1 / this.num2;
+  }
+
+  calcular(): number {
+    if (this.operacion === 'sumar') {
+      this.onSumar();
+      return this.resultado;
+    } else if (this.operacion === 'restar') {
+      this.onRestar();
+      return this.resultado;
+    } else if (this.operacion === 'multiplicar') {
+      this.onMultiplicar();
+      return this.resultado;
+    } else if (this.operacion === 'dividir') {
+      this.onDividir();
+      return this.resultado;
+    }
+
+    return 0;
+  }
 }
+
